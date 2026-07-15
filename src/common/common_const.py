17ec0117923bf7cd43f15d9e1f98cddc67bf5d49
kimha <khn09172830@gmail.com>
2026-07-15 12:51:55 +0900

    holiday_url 추가

--- src/common/common_const.py
+++ src/common/common_const.py
@@ -63,6 +63,8 @@ class StockConstant:
     """
     token_url = "https://openapi.koreainvestment.com:9443/oauth2/tokenP"
     stock_url = "https://openapi.koreainvestment.com:9443/uapi/domestic-stock/v1/quotations/inquire-price"
+    holiday_url = "https://openapi.koreainvestment.com:9443/uapi/domestic-stock/v1/quotations/chk-holiday"
+
 
 
 
